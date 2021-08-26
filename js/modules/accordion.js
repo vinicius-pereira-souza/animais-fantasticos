7120b4a0d403f523cfe28691e56c0982e0670ec4
Vinicius Pereira de Souza <vinicius.pereira.souza98@gmail.com>
2021-08-25 23:17:47 -0300

    retirado console do script

--- js/modules/accordion.js
+++ js/modules/accordion.js
@@ -1,7 +1,7 @@
 export default class Accordion {
   constructor(list) {
     this.accordionList = document.querySelectorAll(list);
-    this.activeClass = 'ativo'
+    this.activeClass = 'ativo';
   }
 
   toogleAccordion(item) {
