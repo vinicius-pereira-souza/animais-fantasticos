6e371adcf52e45ae0861b041626114dffe9a1cfa
Vinicius Pereira de Souza <vinicius.pereira.souza98@gmail.com>
2021-08-25 23:22:39 -0300

    build gerada

--- js/modules/accordion.js
+++ js/modules/accordion.js
@@ -8,18 +8,20 @@ export default class Accordion {
     item.classList.toggle(this.activeClass);
     item.nextElementSibling.classList.toggle(this.activeClass);
   }
+
   // adiciona os eventos ao accordion
   addAccordionEvent() {
     this.accordionList.forEach((item) => {
-      item.addEventListener('click', () =>  this.toogleAccordion(item));
+      item.addEventListener('click', () => this.toogleAccordion(item));
     });
   }
+
   // iniciar função
   init() {
-    if (this.accordionList.length){
+    if (this.accordionList.length) {
       // ativar primeiro item
-      this.toogleAccordion(this.accordionList[0])
-      this.addAccordionEvent()
+      this.toogleAccordion(this.accordionList[0]);
+      this.addAccordionEvent();
     }
     return this;
   }
